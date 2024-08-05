ff5ef967f640084aa9f467319b97426dc14d6e25
Abhizerk <vtu16139@veltech.edu.in>
2024-08-06 02:41:09 +0530

    added image feature

--- script.js
+++ script.js
@@ -23,17 +23,21 @@ document.getElementById('search-bar').addEventListener('input', function() {
 
 document.getElementById('search-button').addEventListener('click', function() {
     const searchQuery = document.getElementById('search-bar').value;
-    const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&titles=${searchQuery}&prop=extracts&exintro&explaintext`;
+    const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&titles=${searchQuery}&prop=extracts|info|pageimages&inprop=url&exintro&explaintext&piprop=thumbnail&pithumbsize=200`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const page = Object.values(data.query.pages)[0];
-            const extract = page.extract;
 
-            if (extract) {
+            if (page) {
                 const tableBody = document.querySelector('#results-table tbody');
                 tableBody.innerHTML = `
+                    <tr>
+                        <td>Image</td>
+                        <td>${page.thumbnail ? `<img src="${page.thumbnail.source}" alt="${searchQuery}">` : 'No image available'}</td>
+                        <td></td>
+                    </tr>
                     <tr>
                         <td>Full Name</td>
                         <td>${searchQuery}</td>
@@ -41,7 +45,67 @@ document.getElementById('search-button').addEventListener('click', function() {
                     </tr>
                     <tr>
                         <td>Description</td>
-                        <td>${extract}</td>
+                        <td>${page.extract || 'N/A'}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Page ID</td>
+                        <td>${page.pageid}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Last Edited</td>
+                        <td>${new Date(page.touched).toLocaleString()}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Full URL</td>
+                        <td><a href="${page.fullurl}" target="_blank">${page.fullurl}</a></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Background</td>
+                        <td>Background information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Occupation</td>
+                        <td>Occupation information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Education</td>
+                        <td>Education information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Interests and Hobbies</td>
+                        <td>Interests and hobbies information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Family</td>
+                        <td>Family information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Achievements</td>
+                        <td>Achievements information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Social Media</td>
+                        <td>Social media presence information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Personality Traits</td>
+                        <td>Personality traits information here</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Goals and Ambitions</td>
+                        <td>Goals and ambitions information here</td>
                         <td></td>
                     </tr>
                 `;
